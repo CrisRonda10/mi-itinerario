7b0bbb69e85dd2b3c1296931d3eeadbc88999fd8
CrisRonda10 <100365960+CrisRonda10@users.noreply.github.com>
2023-09-11 12:46:30 -0300

    Auth (#8)
    * sprint 4 para enviar
    
    * modificacion en activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,23 +1,31 @@
-import {useSelector, useDispatch} from "react-redux"
-import { useEffect } from "react"
-import activity_actions from "../store/actions/activities"
 
-const {read_activities_from_itinerary} = activity_actions
+//import { useSelector, useDispatch } from "react-redux"
+import { useEffect,useState } from "react"
+//import activity_actions from "../store/actions/activities"
+import axios from "axios"
+import apiUrl from "../apiUrl"
+
+//const { read_activities_from_itinerary } = activity_actions
 
 
-export default function Activities({itinerary_id}) {
+export default function Activities({ itinerary_id }) {
 
-    const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-    useEffect(
-        ()=>{dispatch(read_activities_from_itinerary({itinerary_id:itinerary_id}))},
-        []
-        )  
-        const activities_from_itinerary = useSelector(store=> store.activities.activities_from_itinerary)
-        console.log(activities_from_itinerary);  
-
+  // useEffect(
+  //   () => { dispatch(read_activities_from_itinerary({ itinerary_id: itinerary_id })) },
+  //   []
+  // )
+  // const activities_from_itinerary = useSelector(store => store.activities.activities_from_itinerary)
+  // console.log(activities_from_itinerary);
+const [activity, setActivity] = useState([])
+useEffect(()=>{
+  axios(apiUrl+'activities?itinerary_id='+itinerary_id)
+  .then(res=>setActivity(res.data.response))
+  .catch(err=> console.log(err))
+},[]);
 
   return (
-    <div className="flex flex-col items-center">{activities_from_itinerary?.map(each=><div className="wrap m-7 w-[230px] h-[174px] border border-black " key={each._id}><img src={each.photo} alt={each._id} className="w-[230px] h-[150px]" /><p className="text-center">{each.name}</p></div>)}</div>
+    <div className="flex flex-col items-center">{activity?.map(each => <div className="wrap m-7 w-[230px] h-[174px] border border-black " key={each._id}><img src={each.photo} alt={each._id} className="w-[230px] h-[150px]" /><p className="text-center">{each.name}</p></div>)}</div>
   )
 }
